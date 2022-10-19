fe793b45edbdf11d99639f3557cb54c5bd34745a
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2022-10-19 13:17:52 +0800

    Update update-xlite.sh

--- update-xlite.sh
+++ update-xlite.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+# Updater Script
+# By Vinstechmy
+#-----------------------------
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
+NC='\e[0m'
+echo ""
+echo -e "[ ${green}INFO${NC} ] Autoscript Update Begin . . ."
+sleep 1
+cd /usr/bin
+rm nf
+
+cd /usr/bin
+wget -O nf "https://raw.githubusercontent.com/vinstechmy/MediaUnlockerTest/main/media.sh" && chmod +x nf
+
+echo -e "[ ${green}INFO${NC} ] Successfully Install New Update File !"
+sleep 1

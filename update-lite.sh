904d1cb9c1a28816bdd85e3ae204db69ddac5000
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2022-10-21 19:23:27 +0800

    Update update-lite.sh

--- update-lite.sh
+++ update-lite.sh
@@ -11,6 +11,7 @@ echo ""
 echo -e "[ ${green}INFO${NC} ] Autoscript Update Begin . . ."
 sleep 1
 cd /usr/bin
+rm xp
 rm menu
 rm nf
 rm restore
@@ -18,6 +19,7 @@ rm backup
 rm status
 
 cd /usr/bin
+wget -O xp "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/xp.sh" && chmod +x xp
 wget -O menu "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/menu.sh" && chmod +x menu
 wget -O nf "https://raw.githubusercontent.com/vinstechmy/MediaUnlockerTest/main/media.sh" && chmod +x nf
 wget -O restore "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/BACKUP/restore.sh" && chmod +x restore

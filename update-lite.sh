1a2e821b2b7ab5f9c07d9e793e1ac73ba4a7962c
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2022-10-28 22:08:57 +0800

    Update update-lite.sh

--- update-lite.sh
+++ update-lite.sh
@@ -11,53 +11,57 @@ echo ""
 echo -e "[ ${green}INFO${NC} ] Autoscript Update Begin . . ."
 sleep 1
 echo ""
+#echo -e "[ ${green}INFO${NC} ] Update Autoscript For Fix Error :
+#
+#✅ Backup & Restore
+#✅ Change Domain Error
+#✅ Others Files For XRAY Account
+#✅ Fix Bugs On Some Commands
+#✅ Fix Netflix Checker
+#✅ Fix DNS Changer
+#"
 echo -e "[ ${green}INFO${NC} ] Update Autoscript For Fix Error :
 
-✅ Backup & Restore
-✅ Change Domain Error
-✅ Others Files For XRAY Account
-✅ Fix Bugs On Some Commands
-✅ Fix Netflix Checker
-✅ Fix DNS Changer
+✅ New Menu UI With Bandwith Monitor
 "
 sleep 3
-rm /root/.config/rclone/rclone.conf
+#rm /root/.config/rclone/rclone.conf
 cd /usr/bin
-rm dns
-rm xp
-rm add-host
-rm add-trgr
-rm add-vmgr
-rm add-vlgr
-rm add-tr
-rm add-vless
-rm add-ws
-rm user-tr
-rm user-vless
+#rm dns
+#rm xp
+#rm add-host
+#rm add-trgr
+#rm add-vmgr
+#rm add-vlgr
+#rm add-tr
+#rm add-vless
+#rm add-ws
+#rm user-tr
+#rm user-vless
 rm menu
-rm nf
-rm restore
-rm backup
-rm status
+#rm nf
+#rm restore
+#rm backup
+#rm status
 
 cd /usr/bin
-wget -O add-host "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/add-host.sh" && chmod +x add-host
-wget -O dns "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/dns.sh" && chmod +x dns
-wget -O xp "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/xp.sh" && chmod +x xp
-wget -O add-trgr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/GRPC/add-trgr.sh" && chmod +x add-trgr
-wget -O add-vmgr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/GRPC/add-vmgr.sh" && chmod +x add-vmgr
-wget -O add-vlgr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/GRPC/add-vlgr.sh" && chmod +x add-vlgr
-wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-tr.sh" && chmod +x add-tr
-wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-vless.sh" && chmod +x add-vless
-wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-ws.sh" && chmod +x add-ws
-wget -O user-tr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/user-tr.sh" && chmod +x user-tr
-wget -O user-vless "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/user-vless.sh" && chmod +x user-vless
+#wget -O add-host "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/add-host.sh" && chmod +x add-host
+#wget -O dns "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/dns.sh" && chmod +x dns
+#wget -O xp "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/xp.sh" && chmod +x xp
+#wget -O add-trgr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/GRPC/add-trgr.sh" && chmod +x add-trgr
+#wget -O add-vmgr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/GRPC/add-vmgr.sh" && chmod +x add-vmgr
+#wget -O add-vlgr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/GRPC/add-vlgr.sh" && chmod +x add-vlgr
+#wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-tr.sh" && chmod +x add-tr
+#wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-vless.sh" && chmod +x add-vless
+#wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-ws.sh" && chmod +x add-ws
+#wget -O user-tr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/user-tr.sh" && chmod +x user-tr
+#wget -O user-vless "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/user-vless.sh" && chmod +x user-vless
 wget -O menu "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/menu.sh" && chmod +x menu
-wget -O nf "https://raw.githubusercontent.com/vinstechmy/MediaUnlockerTest/main/media.sh" && chmod +x nf
-wget -O restore "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/BACKUP/restore.sh" && chmod +x restore
-wget -O backup "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/BACKUP/backup.sh" && chmod +x backup
-wget -O status "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/status.sh" && chmod +x status
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/OTHERS/rclone.conf"
+#wget -O nf "https://raw.githubusercontent.com/vinstechmy/MediaUnlockerTest/main/media.sh" && chmod +x nf
+#wget -O restore "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/BACKUP/restore.sh" && chmod +x restore
+#wget -O backup "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/BACKUP/backup.sh" && chmod +x backup
+#wget -O status "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/status.sh" && chmod +x status
+#wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/OTHERS/rclone.conf"
 
 echo -e "[ ${green}INFO${NC} ] Successfully Install New Update File !"
 sleep 1

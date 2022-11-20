0f1bf3349c8baa968e3f4c81694050f714ccda0a
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2022-11-20 08:44:38 +0800

    Update update-v2.sh

--- update-v2.sh
+++ update-v2.sh
@@ -43,28 +43,28 @@ rm menu-xray
 rm menu-xtr
 #rm del-ws
 #rm xp
-#wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/OTHERS/rclone.conf"
+#wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/OTHERS/rclone.conf"
 cd /usr/bin
-#wget -O dns "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/SSH/dns.sh" && chmod +x dns
-#wget -O restore "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/BACKUP/restore.sh" && chmod +x restore
+#wget -O dns "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/SSH/dns.sh" && chmod +x dns
+#wget -O restore "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/BACKUP/restore.sh" && chmod +x restore
 wget -O nf "https://raw.githubusercontent.com/vinstechmy/MediaUnlockerTest/main/media.sh" && chmod +x nf
-wget -O menu-tr "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/SSH/menu-tr.sh" && chmod +x menu-tr
-wget -O menu-trgo "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/SSH/menu-trgo.sh" && chmod +x menu-trgo
-wget -O menu-vless "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/SSH/menu-vless.sh" && chmod +x menu-vless
-wget -O menu-ws "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/SSH/menu-ws.sh" && chmod +x menu-ws
-wget -O menu-xray "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/SSH/menu-xray.sh" && chmod +x menu-xray
-wget -O menu-xtr "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/SSH/menu-xtr.sh" && chmod +x menu-xtr
-#wget -O status "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/SSH/status.sh" && chmod +x status
-#wget -O xp "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/SSH/xp.sh" && chmod +x xp
-wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/XRAY/add-tr.sh" && chmod +x add-tr
-wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/XRAY/add-vless.sh" && chmod +x add-vless
-wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/XRAY/add-ws.sh" && chmod +x add-ws
-wget -O user-tr "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/XRAY/user-tr.sh" && chmod +x user-tr
-wget -O user-vless "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/XRAY/user-vless.sh" && chmod +x user-vless
-wget -O user-ws "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/XRAY/user-ws.sh" && chmod +x user-ws
-#wget -O del-ws "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/XRAY/del-ws.sh" && chmod +x del-ws
+wget -O menu-tr "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/SSH/menu-tr.sh" && chmod +x menu-tr
+wget -O menu-trgo "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/SSH/menu-trgo.sh" && chmod +x menu-trgo
+wget -O menu-vless "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/SSH/menu-vless.sh" && chmod +x menu-vless
+wget -O menu-ws "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/SSH/menu-ws.sh" && chmod +x menu-ws
+wget -O menu-xray "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/SSH/menu-xray.sh" && chmod +x menu-xray
+wget -O menu-xtr "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/SSH/menu-xtr.sh" && chmod +x menu-xtr
+#wget -O status "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/SSH/status.sh" && chmod +x status
+#wget -O xp "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/SSH/xp.sh" && chmod +x xp
+wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/XRAY/add-tr.sh" && chmod +x add-tr
+wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/XRAY/add-vless.sh" && chmod +x add-vless
+wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/XRAY/add-ws.sh" && chmod +x add-ws
+wget -O user-tr "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/XRAY/user-tr.sh" && chmod +x user-tr
+wget -O user-vless "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/XRAY/user-vless.sh" && chmod +x user-vless
+wget -O user-ws "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/XRAY/user-ws.sh" && chmod +x user-ws
+#wget -O del-ws "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/XRAY/del-ws.sh" && chmod +x del-ws
 #cd
-#wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium/main/OTHERS/rclone.conf"
+#wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/vinstechmy/MultiportWebsocketPremium-V2/main/OTHERS/rclone.conf"
 
 echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
 sleep 1

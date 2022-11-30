43cbb53fa146d501946ab3f3c28b57a4bb04d859
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2022-11-30 15:37:24 +0800

    Update update-xlite.sh

--- update-xlite.sh
+++ update-xlite.sh
@@ -12,47 +12,45 @@ echo -e "[ ${green}INFO${NC} ] Autoscript Update Begin . . ."
 sleep 1
 echo -e "[ ${green}INFO${NC} ] Update Autoscript For Fix Error :
 
-✅ Backup & Restore
 ✅ Change Domain Error
-✅ Others Files For XRAY Account
-✅ Fix Bugs On Some Commands
-✅ Fix Netflix Checker
-✅ Fix DNS Changer
+✅ Renewcert Error
 "
 sleep 3
 rm /root/.config/rclone/rclone.conf
 cd /usr/bin
-rm nf
-rm menu
-rm dns
-rm add-tr
-rm add-vless
-rm add-ws
-rm add-xtr
-rm add-xray
-rm add-trgo
-rm add-trgr
-rm add-vmgr
-rm add-vlgr
-rm restore
-rm backup
+#rm nf
+#rm menu
+#rm dns
+#rm add-tr
+#rm add-vless
+#rm add-ws
+#rm add-xtr
+#rm add-xray
+#rm add-trgo
+#rm add-trgr
+#rm add-vmgr
+#rm add-vlgr
+#rm restore
+#rm backup
 
 cd /usr/bin
-wget -O nf "https://raw.githubusercontent.com/vinstechmy/MediaUnlockerTest/main/media.sh" && chmod +x nf
-wget -O menu "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/SSH/menu.sh" && chmod +x menu
-wget -O dns "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/SSH/dns.sh" && chmod +x dns
-wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/XRAY/add-tr.sh" && chmod +x add-tr
-wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/XRAY/add-vless.sh" && chmod +x add-vless
-wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/XRAY/add-ws.sh" && chmod +x add-ws
-wget -O add-trgr "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/GRPC/add-trgr.sh" && chmod +x add-trgr
-wget -O add-vmgr "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/GRPC/add-vmgr.sh" && chmod +x add-vmgr
-wget -O add-vlgr "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/GRPC/add-vlgr.sh" && chmod +x add-vlgr
-wget -O add-xray "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/XRAY/add-xray.sh" && chmod +x add-xray
-wget -O add-xtr "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/XRAY/add-xtr.sh" && chmod +x add-xtr
-wget -O add-trgo "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/TRGO/add-trgo.sh" && chmod +x add-trgo
-wget -O restore "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/BACKUP/restore.sh" && chmod +x restore
-wget -O backup "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/BACKUP/backup.sh" && chmod +x backup
+wget -O add-host "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/SSH/add-host.sh" && chmod +x add-host
+wget -O certxray "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/XRAY/cert.sh" && chmod +x certxray
+#wget -O nf "https://raw.githubusercontent.com/vinstechmy/MediaUnlockerTest/main/media.sh" && chmod +x nf
+#wget -O menu "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/SSH/menu.sh" && chmod +x menu
+#wget -O dns "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/SSH/dns.sh" && chmod +x dns
+#wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/XRAY/add-tr.sh" && chmod +x add-tr
+#wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/XRAY/add-vless.sh" && chmod +x add-vless
+#wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/XRAY/add-ws.sh" && chmod +x add-ws
+#wget -O add-trgr "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/GRPC/add-trgr.sh" && chmod +x add-trgr
+#wget -O add-vmgr "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/GRPC/add-vmgr.sh" && chmod +x add-vmgr
+#wget -O add-vlgr "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/GRPC/add-vlgr.sh" && chmod +x add-vlgr
+#wget -O add-xray "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/XRAY/add-xray.sh" && chmod +x add-xray
+#wget -O add-xtr "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/XRAY/add-xtr.sh" && chmod +x add-xtr
+#wget -O add-trgo "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/TRGO/add-trgo.sh" && chmod +x add-trgo
+#wget -O restore "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/BACKUP/restore.sh" && chmod +x restore
+#wget -O backup "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/BACKUP/backup.sh" && chmod +x backup
 cd
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/OTHERS/rclone.conf"
+#wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/vinstechmy/XRAY-Lite/main/OTHERS/rclone.conf"
 echo -e "[ ${green}INFO${NC} ] Successfully Install New Update File !"
 sleep 1

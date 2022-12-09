8187d5a0d31422ae99b4ac0d2286e0a6d9bb85b8
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2022-12-09 21:12:31 +0800

    Update update-lite.sh

--- update-lite.sh
+++ update-lite.sh
@@ -1,68 +1,67 @@
 #!/bin/bash
-# Updater Script
-# By Vinstechmy
-#-----------------------------
+#Script Updater By Vinstechmy
 red='\e[1;31m'
 green='\e[0;32m'
 purple='\e[0;35m'
 orange='\e[0;33m'
 NC='\e[0m'
-echo ""
-echo -e "Autoscript Websocket Lite By ${green}Vinstechmy${NC}"
-echo -e "[ ${green}INFO${NC} ] Autoscript Update Begin . . ."
-sleep 1
-echo ""
-echo -e "[ ${green}INFO${NC} ] Update Autoscript For Fix Error :
 
-✅ Minor Updates
-"
-#echo -e "[ ${green}INFO${NC} ] Update Autoscript For Fix Error :
-#
-#✅ New Menu UI With Bandwith Monitor
-#"
-sleep 3
-#rm /root/.config/rclone/rclone.conf
-cd /usr/bin
-#rm dns
-#rm xp
-#rm add-host
-#rm add-trgr
-#rm add-vmgr
-#rm add-vlgr
-#rm add-tr
-#rm add-vless
-#rm add-ws
-#rm user-ws
-#rm user-vless
-rm menu
-#rm nf
-#rm restore
-#rm backup
-#rm status
+if [[ -e /usr/local/bin/xray.bak ]]; then
+     echo ""
+     echo -e "[ ${green}INFO${NC} ] XRAY Core CF-RAY Already Install !"
+	 sleep 1
+	 clear
+	 echo -e "[ ${green}INFO${NC} ] Skip Update XRAY Core & Proceed Autoscript Patch Update . . ."
+	 echo ""
+	 echo -e "[ ${green}INFO${NC} ] Update Starting Now . . ."
+	 echo ""
+	 sleep 1
+	 cd /usr/bin
+	 rm menu
+	 rm add-tr
+     rm add-vless
+     rm add-ws
+     rm user-tr
+     rm user-vless
+	 wget -O menu "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/menu.sh" && chmod +x menu
+	 wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-tr.sh" && chmod +x add-tr
+	 wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-vless.sh" && chmod +x add-vless
+	 wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-ws.sh" && chmod +x add-ws
+	 wget -O user-tr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/user-tr.sh" && chmod +x user-tr
+	 wget -O user-vless "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/user-vless.sh" && chmod +x user-vless
+	 echo ""
+	 echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
+	 sleep 1
+else
+     echo -e "[ ${green}INFO${NC} ] XRAY Core CF-RAY Update Starting !"
+	 sleep 1
+     mv /usr/local/bin/xray /usr/local/bin/xray.bak && wget -q -O /usr/local/bin/xray "https://github.com/dharak36/Xray-core/releases/download/v1.0.0/xray.linux.64bit" && chmod 755 /usr/local/bin/xray
+	 echo ""
+     echo -e "[ ${green}INFO${NC} ] XRAY Core CF-RAY Successfully Install !"
+	 echo ""
+	 echo -e "[ ${green}INFO${NC} ] Please ${red}Restart${NC} All VPN Services After Autoscript Patch Update Is Done . . ."
+	 sleep 3
+	 clear
+	 echo -e "[ ${green}INFO${NC} ] Proceed Autoscript Patch Update . . ."
+	 echo ""
+	 echo -e "[ ${green}INFO${NC} ] Update Starting Now . . ."
+	 echo ""
+	 sleep 1
+	 cd /usr/bin
+	 rm menu
+	 rm add-tr
+     rm add-vless
+     rm add-ws
+     rm user-tr
+     rm user-vless
+	 wget -O menu "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/menu.sh" && chmod +x menu
+	 wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-tr.sh" && chmod +x add-tr
+	 wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-vless.sh" && chmod +x add-vless
+	 wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-ws.sh" && chmod +x add-ws
+	 wget -O user-tr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/user-tr.sh" && chmod +x user-tr
+	 wget -O user-vless "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/user-vless.sh" && chmod +x user-vless
+	 echo ""
+	 echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
+	 sleep 1
 
-cd /usr/bin
-#wget -O add-host "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/add-host.sh" && chmod +x add-host
-#wget -O dns "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/dns.sh" && chmod +x dns
-#wget -O xp "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/xp.sh" && chmod +x xp
-#wget -O add-trgr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/GRPC/add-trgr.sh" && chmod +x add-trgr
-#wget -O add-vmgr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/GRPC/add-vmgr.sh" && chmod +x add-vmgr
-#wget -O add-vlgr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/GRPC/add-vlgr.sh" && chmod +x add-vlgr
-#wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-tr.sh" && chmod +x add-tr
-#wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-vless.sh" && chmod +x add-vless
-#wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/add-ws.sh" && chmod +x add-ws
-#wget -O user-tr "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/user-tr.sh" && chmod +x user-tr
-#wget -O user-ws "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/user-ws.sh" && chmod +x user-ws
-#wget -O user-vless "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/XRAY/user-vless.sh" && chmod +x user-vless
-wget -O menu "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/menu.sh" && chmod +x menu
-wget -O ins-helium "https://raw.githubusercontent.com/vinstechmy/AdsBlock/main/ins-helium.sh" && chmod +x ins-helium
-wget -O bbr "https://raw.githubusercontent.com/vinstechmy/Vinstechmy-TCP-BBR/main/bbr.sh" && chmod +x bbr
-wget -O wssgen "https://raw.githubusercontent.com/vinstechmy/WSS-Converter-Universal/main/wssgen.sh" && chmod +x wssgen
-#wget -O nf "https://raw.githubusercontent.com/vinstechmy/MediaUnlockerTest/main/media.sh" && chmod +x nf
-#wget -O restore "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/BACKUP/restore.sh" && chmod +x restore
-#wget -O backup "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/BACKUP/backup.sh" && chmod +x backup
-#wget -O status "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/SSH/status.sh" && chmod +x status
-cd
-#wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/vinstechmy/AutoscriptWebsocketLite/main/OTHERS/rclone.conf"
-
-echo -e "[ ${green}INFO${NC} ] Successfully Install New Update File !"
-sleep 1
+fi

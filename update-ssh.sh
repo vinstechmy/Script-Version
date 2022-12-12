3834048b123c1c7c14806c0dbb79471961ee2fd7
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2022-12-12 21:37:01 +0800

    Update update-ssh.sh

--- update-ssh.sh
+++ update-ssh.sh
@@ -1,68 +1,71 @@
 #!/bin/bash
-# Updater Script
-# By Vinstechmy
-#-----------------------------
+#Script Updater By Vinstechmy
 red='\e[1;31m'
 green='\e[0;32m'
 purple='\e[0;35m'
 orange='\e[0;33m'
 NC='\e[0m'
-echo ""
-echo -e "[ ${green}INFO${NC} ] Autoscript Update Begin . . ."
-sleep 1
-echo ""
-#echo -e "[ ${green}INFO${NC} ] Update Autoscript For Fix Error :
-#
-#✅ Backup & Restore
-#✅ Change Domain Error
-#✅ Others Files For XRAY Account
-#✅ Fix Bugs On Some Commands
-#✅ Fix Netflix Checker
-#✅ Fix DNS Changer
-#"
-echo -e "[ ${green}INFO${NC} ] Update Autoscript For Fix Error :
 
-✅ Replacing Old Files
-"
-sleep 3
-rm /root/.config/rclone/rclone.conf
-cd /usr/bin
-rm trial
-rm usernew
-rm dns
-rm xp
-rm certxray
-rm add-host
-rm add-tr
-rm add-vless
-rm add-ws
-rm user-tr
-rm user-vless
-rm menu
-rm nf
-rm restore
-rm backup
-rm status
+if [[ -e /usr/local/bin/xray.bak ]]; then
+   echo ""
+   echo -e "[ ${green}INFO${NC} ] XRAY Core CF-RAY Already Install !"
+	 sleep 1
+	 clear
+	 echo -e "[ ${green}INFO${NC} ] Skip Update XRAY Core & Proceed Autoscript Patch Update . . ."
+	 echo ""
+	 echo -e "[ ${green}INFO${NC} ] Update Starting Now . . ."
+	 echo ""
+	 sleep 1
+	 cd /usr/bin
+	 rm bbr
+	 rm menu
+	 rm add-tr
+   rm add-vless
+   rm add-ws
+   rm user-tr
+   rm user-vless
+   rm bbr
+	 wget -O menu "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSH/menu.sh" && chmod +x menu
+	 wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/add-tr.sh" && chmod +x add-tr
+	 wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiporte/main/XRAY/add-vless.sh" && chmod +x add-vless
+	 wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/add-ws.sh" && chmod +x add-ws
+	 wget -O user-tr "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/user-tr.sh" && chmod +x user-tr
+	 wget -O user-vless "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/user-vless.sh" && chmod +x user-vless
+	 wget -O bbr "https://raw.githubusercontent.com/vinstechmy/Vinstechmy-TCP-BBR/main/bbr.sh" && chmod +x bbr
+	 echo ""
+	 echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
+	 sleep 1
+else
+   echo -e "[ ${green}INFO${NC} ] XRAY Core CF-RAY Update Starting !"
+	 sleep 1
+   mv /usr/local/bin/xray /usr/local/bin/xray.bak && wget -q -O /usr/local/bin/xray "https://github.com/dharak36/Xray-core/releases/download/v1.0.0/xray.linux.64bit" && chmod 755 /usr/local/bin/xray
+	 echo ""
+   echo -e "[ ${green}INFO${NC} ] XRAY Core CF-RAY Successfully Install !"
+	 echo ""
+	 echo -e "[ ${green}INFO${NC} ] Please ${red}Restart${NC} All VPN Services After Autoscript Patch Update Is Done . . ."
+	 sleep 5
+	 clear
+	 echo -e "[ ${green}INFO${NC} ] Proceed Autoscript Patch Update . . ."
+	 echo ""
+	 echo -e "[ ${green}INFO${NC} ] Update Starting Now . . ."
+	 echo ""
+	 sleep 1
+	 cd /usr/bin
+	 rm menu
+	 rm add-tr
+   rm add-vless
+   rm add-ws
+   rm user-tr
+   rm user-vless
+	 wget -O menu "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSH/menu.sh" && chmod +x menu
+	 wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/add-tr.sh" && chmod +x add-tr
+	 wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/add-vless.sh" && chmod +x add-vless
+	 wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/add-ws.sh" && chmod +x add-ws
+	 wget -O user-tr "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/user-tr.sh" && chmod +x user-tr
+	 wget -O user-vless "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/user-vless.sh" && chmod +x user-vless
+	 wget -O bbr "https://raw.githubusercontent.com/vinstechmy/Vinstechmy-TCP-BBR/main/bbr.sh" && chmod +x bbr
+	 echo ""
+	 echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
+	 sleep 1
 
-cd /usr/bin
-wget -O trial "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSH/trial.sh" && chmod +x trial
-wget -O usernew "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSH/usernew.sh" && chmod +x usernew
-wget -O add-host "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSH/add-host.sh" && chmod +x add-host
-wget -O dns "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSH/dns.sh" && chmod +x dns
-wget -O xp "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSH/xp.sh" && chmod +x xp
-wget -O certxray "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/cert.sh" && chmod +x certxray
-wget -O add-tr "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/add-tr.sh" && chmod +x add-tr
-wget -O add-vless "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/add-vless.sh" && chmod +x add-vless
-wget -O add-ws "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/add-ws.sh" && chmod +x add-ws
-wget -O user-tr "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/user-tr.sh" && chmod +x user-tr
-wget -O user-vless "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/XRAY/user-vless.sh" && chmod +x user-vless
-wget -O menu "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSH/menu.sh" && chmod +x menu
-wget -O nf "https://raw.githubusercontent.com/vinstechmy/MediaUnlockerTest/main/media.sh" && chmod +x nf
-wget -O restore "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/BACKUP/restore.sh" && chmod +x restore
-wget -O backup "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/BACKUP/backup.sh" && chmod +x backup
-wget -O status "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSH/status.sh" && chmod +x status
-cd
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/OTHERS/rclone.conf"
-
-echo -e "[ ${green}INFO${NC} ] Successfully Install New Update File !"
-sleep 1
+fi
